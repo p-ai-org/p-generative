0cd360690fb9471089d5a93f3151c26754064af9
Jacob Zimmerman <jczimm@jczimm.com>
2022-04-29 00:18:04 -0700

    treat shape-position modeling as a hierarchical model, incorporate noise into shape positions for outlining to get more helpful scoring

--- shapes-vision.wppl.js
+++ shapes-vision.wppl.js
@@ -1,14 +1,14 @@
 // old comments block now in shapes-vision-notes.txt
 
 var seed = randomInteger(1000000000)
-// var seed = 797436640
+// var seed = 987908295 // for flowers image, found a circle well!
 util.seedRNG(seed)
 display('seed is ' + seed)
 
 var drawShapes = function(canvas, shapeAndColorData) {
   var shapes = shapeAndColorData.shapes;
   var colorings = shapeAndColorData.shapeColors;
-  
+
   if (shapes.length == 0) { return; }
 
   var next = shapes[0];
@@ -18,7 +18,7 @@ var drawShapes = function(canvas, shapeAndColorData) {
   var stroke = coloring ? 'rgba(1,1,1,0)' : 'black';
   var opacity = coloring ? coloring.opacity : 1.0;
 
-  var outlineThickness = .5;
+  var outlineThickness = 1;
   if (next.shape === 'rect') {
     var leftX = next.x - next.dims[0]
     var topY = next.y - next.dims[1]
@@ -41,70 +41,73 @@ var rgbFix = function(value) {
 
 var makeColors = function(n, colors) {
   if (n == 0) return colors
-  
+
   var redVal = [255, 200, 235, 120, 0][randomInteger(5)]
   var noisedRedVal = rgbFix(redVal + gaussian(0, 10))
-  
+
   var greenVal = [0, 100, 235, 120, 50][randomInteger(5)]
   var noisedGreenVal = rgbFix(greenVal + gaussian(0, 10))
-  
+
   var blueVal = [0, 10, 235, 190, 255][randomInteger(5)]
   var noisedBlueVal = rgbFix(blueVal + gaussian(0, 10))
-  
+
   var colorString = "rgb("+noisedRedVal+","+noisedGreenVal+","+noisedBlueVal+")"
   var color = colorString
-  
+
   //var color = ["red", "blue", "cyan", "green", "yellow", "white", "pink", "black", "orange"][randomInteger(8)]
   return makeColors(n - 1, colors.concat([{
     fill: color,
     stroke: color,
     opacity: 1.0
-  }]))                   
+  }]))
+
 }
 
-var makeRandShapes = function(n, shapes, targetImage, prevScore, sampleDiversity) {  
+var makeRandShapes = function(n, shapes, targetImage, prevScore, sampleDiversity) { 
+  // categorical distribution of the shape type is 
   var rectP = uniform(0, 1)
   var circleP = uniform(0, 1-rectP)
   var triP = uniform(0, 1-rectP-circleP)
   var shapeType = categorical({ ps: [rectP, circleP, triP], vs: ['rect', 'circle', 'tri'] })
-  
-  var x = randomInteger(imgWidth+5*2)-5
-  var y = randomInteger(imgHeight+5*2)-5
+
+  // x is distance from left edge, y is distance from top edge
+  var trueX = gaussian({ mu: imgWidth/2, sigma: imgWidth/5 }) // randomInteger(imgWidth+5*2)-5
+  var trueY = gaussian({ mu: imgHeight/2, sigma: imgHeight/5 }) // randomInteger(imgHeight+5*2)-5
   var dim1 = randomInteger(imgWidth-10*2)+10
   var dim2 = shapeType === 'circle' ? dim1 : randomInteger(imgHeight-10*2)+10
+  
+  var dim1b = shapeType === 'tri' ? ((flip() ? -1 : 1) * randomInteger(imgWidth-10*2)+10) : 0
+  var dim2b = shapeType === 'tri' ? ((flip() ? -1 : 1) * randomInteger(imgHeight-10*2)+10) : 0
   
   // while we can get all we need from just between 0 and 90,
   // allowing for values between 0 and 360 gives the model a bit more flexibility to be able to rotate by changing just one parameter
   var angle = shapeType === 'rect' ? randomInteger(360) : 0
+  
+  var createShape = mem(function(type, n) {
+    return Infer({ method: 'forward', samples: 30, model() {
+      var x = trueX + uniform(-dim1/3, dim1/3)
+      var y = trueY + uniform(-dim2/3, dim2/3)
+      return (
+        type === 'rect' ?
+        { shape: type, dims: [dim1, dim2], x, y, angle }
+        : type === 'circle' ?
+        { shape: type, radius: dim1/2, x, y }
+        : type === 'tri' ?
+        {
+          shape: type,
+          xs: [x - dim1/2, x + dim1/2, x+dim1b/2],
+          ys: [y - dim2/2, y + dim2/2, y+dim2b/2]
+          // the third component gives the triangle three degrees of variability for each axis
+        }
+        : null)
+    } })
+  })
 
-  var newShape =
-    shapeType === 'rect' ?
-    {
-      shape: shapeType,
-      dims: [dim1, dim2],
-      x, // distance from left edge
-      y, // distance from top edge
-      angle // angle is in degrees
-    }
-    : shapeType === 'circle' ?
-    {
-      shape: shapeType,
-      radius: dim1/2,
-      x, // distance from left edge
-      y // distance from top edge
-    }
-    : shapeType === 'tri' ?
-    {
-        shape: shapeType,
-        xs: [x - dim1/2, x + dim1/2, randomInteger(imgWidth+5*2)-5], // each vertex's distance from left edge
-        ys: [y - dim2/2, y + dim2/2, randomInteger(imgHeight+5*2)-5] // each vertex's distance from top edge
-      // the third component gives the triangle three degrees of variability for each axis
-    }
-    : null
-  var newShapes = shapes.concat([newShape])
+  var shapeCreator = createShape(shapeType)
+  var newShapes = shapes.concat([sample(shapeCreator)])
 
   if (targetImage) {
-    var show = flip(0.05)
+    var show = true // flip(0.05)
     var generatedImage = Draw(imgWidth, imgHeight, show)
     generatedImage.rectangle(0,0,imgWidth,imgHeight,'white','white')
     var shapeColors = repeat(newShapes.length, function() {return undefined}) // dummy
@@ -123,79 +126,75 @@ var makeRandShapes = function(n, shapes, targetImage, prevScore, sampleDiversity
 
   return (n==1) ? {shapes: newShapes, shapeType} : makeRandShapes(n - 1, newShapes)
 }
-    
+
 // our inference loop is run twice as two different versions: once for finding outlines (findOutlines), and one for finding colors (findShapeColors)
 // we are doing these two inference steps independently rather than jointly to model how the brain has lower-level processing before higher-level processing
 
 
 var outliner = function(trueEdges) {
-  var sampleDiversity = 10000
+  var sampleDiversity = 1000
   // var distanceNoise = 0.001
 
-//   var counter = []
-//   var showEveryN = 100
+  //   var counter = []
+  //   var showEveryN = 100
   var findOutlines = function() {
     var numShapes = 10 // randomInteger(11)
-    
+
     // inside makeRandShapes, conditioning inference using edges data (integrating lower-level contrast information)
     var randShapes = makeRandShapes(numShapes, [], trueEdges, 0, sampleDiversity)
-    // if not show, canvas is still being made inside makeRandShapes and factor is happening there
-    
-    // conditioning inference using edges data (integrating lower-level contrast information)
-//     var randShapes = makeRandShapes(numShapes, [])
+
     var shapes = randShapes.shapes
     var shapeType = randShapes.shapeType
     var shapeColors = repeat(numShapes, function() {return undefined}) // dummy
 
-//     var show = counter.length % showEveryN == 0
-//     if (show) {
-//       var canvas1 = Draw(imgWidth, imgHeight, true)
-//       canvas1.rectangle(0,0,imgWidth,imgHeight,'white','white')
-//       drawShapes(canvas1, {shapes, shapeColors})
-//       var score = -(canvas1.distance(trueEdges)) // + gaussian(0, distanceNoise))
-//     // //   display(score)
-//       factor(score/sampleDiversity)
-//     }
+    //     var show = counter.length % showEveryN == 0
+    //     if (show) {
+    //       var canvas1 = Draw(imgWidth, imgHeight, true)
+    //       canvas1.rectangle(0,0,imgWidth,imgHeight,'white','white')
+    //       drawShapes(canvas1, {shapes, shapeColors})
+    //       var score = -(canvas1.distance(trueEdges)) // + gaussian(0, distanceNoise))
+    //     // //   display(score)
+    //       factor(score/sampleDiversity)
+    //     }
 
-//     counter.push(1)
-    
+    //     counter.push(1)
+    // if not show, canvas is still being made inside makeRandShapes and factor is happening there
+
     return {shapes, shapeColors, shapeType, numShapes}
   }
   return findOutlines
 }
 
 var painter = function(targetimage, outlinesDist) {
-  var sampleDiversity = 100
+  var sampleDiversity = 1000
   var distanceNoise = 0.001
 
   var counter = []
   var showEveryN = 100
   var findShapeColors = function() {
-    
+
     // var shapes = makeRandShapes(numShapes, [])
     // condition inference using outlines data (integrating outlines, based on lower-level contrast information)
     // observe(outlinesDist, [shapes, repeat(numShapes, function() {return undefined})])
-    
+
     var shapes = sample(outlinesDist).shapes
-    // var shapes = foundOutlines[0]
-
-    var show = counter.length % showEveryN == 0
-    var canvas1 = Draw(imgWidth, imgHeight, show)
-
     // condition inference using target image data (integrating lower-level color information)
     var numShapes = shapes.length
     var shapeColors = makeColors(numShapes, [])
+    
+    var show = counter.length % showEveryN == 0
+    var canvas1 = Draw(imgWidth, imgHeight, show)
     drawShapes(canvas1, { shapes, shapeColors })
     var score = -(canvas1.distance(targetimage)) // + gaussian(0, distanceNoise))
-  //   display(score)
+    //   display(score)
     factor(score/sampleDiversity)
 
-//     if (!show) {
-//       canvas1.destroy()
-//     }
+    if (!show) {
+      canvas1.destroy()
+    }
 
     counter.push(1)
-    
+
     return {shapes, shapeColors}
   }
   return findShapeColors
@@ -219,7 +218,7 @@ var edgePixels = detectEdges(targetimage, edgeThreshold)
 trueEdges.setImageData(edgePixels)
 
 // var outlines = Infer({ method: 'MCMC', samples: 800, model: outliner(trueEdges), onlyMAP: false })
-var outlines = Infer({ method: 'SMC', particles: 100, rejuvSteps: 0, model: outliner(trueEdges), onlyMAP: false })
+var outlines = Infer({ method: 'SMC', particles: 100, rejuvSteps: 5, model: outliner(trueEdges), onlyMAP: false })
 
 // draw best outlines
 var chooseABest = function(dist) {
@@ -235,6 +234,8 @@ var bestOutlines = chooseABest(outlines)
 
 // var bestOutlines = sample(outlines) // if onlyMap: true
 drawShapes(Draw(imgWidth, imgHeight, true), bestOutlines)
+// display(JSON.stringify(bestOutlines))
+
 display('shape type:')
 viz(marginalize(outlines, 'shapeType'))
 display('')
@@ -250,7 +251,7 @@ Draw(imgWidth, imgHeight, true).setImageData(edgePixels)
 // 2. Find colors
 
 // fill in the shapes
-var bestColoredShapes = Infer({ method: 'MCMC', samples: 501, model: painter(targetimage, outlines) })
+var bestColoredShapes = Infer({ method: 'MCMC', samples: 301, model: painter(targetimage, outlines) })
 // samples should not be a multiple of showEveryN, since it might be causing the canvas to be destroyed and then Draw tries to connect to that one
 
 display('done!')
